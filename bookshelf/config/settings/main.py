02f5736f8277410d57cb88845d9c9b87422d39f0
Inderpartap <inderpartap_cheema@sfu.ca>
2021-04-14 18:04:50 -0700

    Change staticfile path

--- bookshelf/config/settings/main.py
+++ bookshelf/config/settings/main.py
@@ -80,8 +80,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.8/howto/static-files/
 
 PROJECT_ROOT   =   os.path.dirname(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
-APP_ROOT = os.path.join(PROJECT_ROOT, 'modules/dashboard/')
-STATIC_ROOT  =   os.path.join(PROJECT_ROOT, 'staticfiles')
+APP_ROOT = os.path.join(PROJECT_ROOT, 'modules/dashboard')
+STATIC_ROOT  =   os.path.join(APP_ROOT, 'static')
 STATIC_URL = '/static/'
 
 
